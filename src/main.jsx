c13aae636b246a8a0c96c7ad6dd72dc5bb1dd241
ccmendezt <camilomendez1903@gmail.com>
2023-05-12 10:50:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,19 @@ import MapView from '@arcgis/core/views/MapView';
 import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
 
 let map = new Map({
-  basemap: "streets-relief-vector" // Tipo de mapa base inicial
+  basemap: "osm" // Tipo de mapa base inicial
 });
 
 const view = new MapView({
   container: "root",
   map: map,
   center: [-74.05121774713942, 4.673863279034983], // Mapa centrado en el edificio de ESRI COLOMBIA en el Chicó
-  zoom: 12
+  zoom: 20 // Nivel de zoom inicial
 });
 
 const cambiarVista = new BasemapToggle({
-  view: view, // Acceso al mapa base "topo-vector" del mapa
-  nextBasemap: "osm" // Mapa base alternativo
+  view: view, // Acceso al mapa base del mapa
+  nextBasemap: "streets-relief-vector" // Mapa base alternativo
 });
 
 // Agregar el widget de cambiar vista al mapa en la esquina superior derecha
